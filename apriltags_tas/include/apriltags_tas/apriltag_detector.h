UniBwTAS/apriltags_tas

commit d8c4768acaabdd59f30ae4171d0533d2e37203b4

Do not detect tags if we do not have any subscribers

apriltags_tas/src/apriltag_detector.cpp
@@ -74,6 +74,13 @@ void AprilTagDetector::imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 void AprilTagDetector::process(const cv::Mat& image)
 {
+    if (detections_pub_.getNumSubscribers() == 0 && image_pub_.getNumSubscribers() == 0)
+    {
+        ROS_WARN_STREAM("No subscribers => Do not detect tags!");
+
+        return;
+    }
+
     cv::Mat gray_image;
     cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
 

commit 7239eb7118bef66010659a36f913283f296b3bf9

Fix test image demo
Got broken in d8c4768

apriltags_tas/include/apriltags_tas/apriltag_detector.h
@@ -24,7 +24,9 @@
 class AprilTagDetector
 {
   public:
-    AprilTagDetector(const sensor_msgs::CameraInfo::ConstPtr& camera_info, apriltag_ros::TagDetector& tag_config);
+    AprilTagDetector(const bool use_test_image,
+                     const sensor_msgs::CameraInfo::ConstPtr& camera_info,
+                     apriltag_ros::TagDetector& tag_config);
 
     void reconfigure(apriltags_tas::AprilTagDetectorConfig& config, uint32_t level);
     void imageCallback(const sensor_msgs::ImageConstPtr& msg);
@@ -51,6 +53,7 @@ class AprilTagDetector
 
     std::shared_ptr<AprilTags::TagDetector> apriltag_cpp_detector_;
 
+    bool use_test_image_;
     image_geometry::PinholeCameraModel camera_model_;
     cv::Mat image_;
     std_msgs::Header img_header_;

apriltags_tas/src/apriltag_detector.cpp
@@ -10,9 +10,10 @@
 
 #include <chrono>
 
-AprilTagDetector::AprilTagDetector(const sensor_msgs::CameraInfo::ConstPtr& camera_info,
+AprilTagDetector::AprilTagDetector(const bool use_test_image,
+                                   const sensor_msgs::CameraInfo::ConstPtr& camera_info,
                                    apriltag_ros::TagDetector& tag_config)
-    : tag_config_(tag_config)
+    : use_test_image_(use_test_image), tag_config_(tag_config)
 {
     if (camera_info)
     {
@@ -74,7 +75,7 @@ void AprilTagDetector::imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 void AprilTagDetector::process(const cv::Mat& image)
 {
-    if (detections_pub_.getNumSubscribers() == 0 && image_pub_.getNumSubscribers() == 0)
+    if (!use_test_image_ && detections_pub_.getNumSubscribers() == 0 && image_pub_.getNumSubscribers() == 0)
     {
         ROS_WARN_STREAM("No subscribers => Do not detect tags!");
 
@@ -160,9 +161,7 @@ void AprilTagDetector::refineCornerPointsByDirectEdgeOptimization(
 
             int line_thickness = 5;
 
-            auto nextCornerIndex = [](const int i) {
-                return (i+1) % 4;
-            };
+            auto nextCornerIndex = [](const int i) { return (i + 1) % 4; };
 
             for (int i = 0; i < 4; i++)
             {
@@ -207,7 +206,8 @@ void AprilTagDetector::refineCornerPointsByDirectEdgeOptimization(
                                           &estimated_edge_normals,
                                           &estimated_edge_offsets,
                                           &numeric_diff_options,
-                                          &nextCornerIndex](const int i) {
+                                          &nextCornerIndex](const int i)
+            {
                 const int pixel_count = cv::countNonZero(mask_images[i]);
 
                 ceres::CostFunction* cost_function =

apriltags_tas/src/apriltag_detector_node.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
 
     apriltag_ros::TagDetector tag_config(nh);
 
-    AprilTagDetector apriltag_detector(camera_info, tag_config);
+    AprilTagDetector apriltag_detector(use_test_input_image, camera_info, tag_config);
 
     dynamic_reconfigure::Server<apriltags_tas::AprilTagDetectorConfig> reconfigure_server;
     reconfigure_server.setCallback(boost::bind(&AprilTagDetector::reconfigure, &apriltag_detector, _1, _2));
@@ -41,7 +41,7 @@ int main(int argc, char** argv)
     image_transport::ImageTransport it(nh);
 
     apriltag_detector.detections_pub_ = nh.advertise<apriltags_msgs::AprilTagDetections>("detections", 1, false);
-    apriltag_detector.image_pub_ = it.advertise("image", 1);
+    apriltag_detector.image_pub_ = it.advertise("image", 1, true);
 
     std::unique_ptr<image_transport::Subscriber> sub;
 